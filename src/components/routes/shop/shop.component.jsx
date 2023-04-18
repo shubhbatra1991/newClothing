ad49263ed573534a60c7f142a88fa41fa4b44089
shubh batra <godwithsam@gmail.com>
2023-04-18 17:23:55 +0100

    updated files.

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,20 +1,33 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
+
+import { getCategoriesAndDocuments } from '../../../utils/firebase/firebase.utils';
+import { setCategoriesMap } from '../../../store/categories/category.action';
 
 
 import './shop.styles.scss';
 
 
-
 const Shop = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+          const categoryMap = await getCategoriesAndDocuments();
+          dispatch(setCategoriesMap(categoryMap));
+        };
+        getCategoriesMap();
+      },[dispatch]);
+
     return(
             <Routes>
                 <Route index element={<CategoriesPreview />} />
                 <Route path=":category" element={<Category />} />
-            </Routes>
-        
+            </Routes> 
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 
 import App from './App';
-// import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.contexts';
 
 import './index.scss';
@@ -17,13 +15,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter >
-        {/* <UserProvider> */}
-          <CategoriesProvider>
-            <CartProvider>
-            <App />
-            </CartProvider>
-          </CategoriesProvider>
-        {/* </UserProvider> */}
+        <CartProvider>
+         <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
